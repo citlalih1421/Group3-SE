143d7b6bc0fc24bacbd95fc83ce2515d92dfb489
gagefulwood <gagetfulwood@gmail.com>
2024-03-13 00:20:31 -0500

    Added WIP shoe, brand, and categry models to core app

--- shoebae/core/views.py
+++ shoebae/core/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.cache import cache
+from . models import Category, Shoe
 
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+def shoe_details(request):
+    shoe = get_object_or_404(Shoe, id)
 
 def search(request):
     return render(request,'search.html')
--- shoebae/shoebae/settings.py
+++ shoebae/shoebae/settings.py
@@ -135,3 +135,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#caching
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'TIMEOUT': 3600,  # Cache timeout in seconds (e.g., 1 hour)
+    }
+}
