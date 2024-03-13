2d6488cc12f35072ec9c3d0451f835b258387f3f
John Taylor <jmt880@msstate.edu>
2024-03-12 19:54:43 -0500

    added buyer and seller models in accounts

--- shoebae/accounts/models.py
+++ shoebae/accounts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+#MODEL FOR SELLERS
+class Seller(models.Model):
+    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+
+#MODEL FOR BUYERS
+class Buyer(models.Model):
+    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
