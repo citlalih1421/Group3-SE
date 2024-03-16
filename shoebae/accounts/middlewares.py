3dbad94adcd7f31e5e8ed75677130ec563f9f58e
Gage Fulwood <gagetfulwood@gmail.com>
2024-03-15 22:26:04 -0500

    Revised files, updated frontend for cleaner look

---
+++ shoebae/accounts/middlewares.py
@@ -0,0 +1,15 @@
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.models import Group
+
+class GroupPermissionsMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if (request.user.is_authenticated):
+            if (request.user.groups.filter(name="Buyer").exists() and not request.user.has_perm('accounts.can_view_buyer')):
+                raise PermissionDenied('You do not have permission to view Buyer content')
+            elif (request.user.groups.filter(name="Seller").exists() and not request.user.has_perm('accounts.can_view_seller')):
+                raise PermissionDenied('You do not have permission to view Seller content')
+        response = self.get_response(request)
+        return response
--- shoebae/accounts/models.py
+++ shoebae/accounts/models.py
@@ -1,28 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import Group as BaseGroup
 
 # Create your models here.
 
-#MODEL FOR SELLERS
-class Seller(models.Model):
-    name = models.CharField(max_length=100)
-    username = models.CharField(max_length=50, unique=True)
-    email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=20)
-    address = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
+class UserType(BaseGroup):
+    description = models.TextField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'User Type'
+        verbose_name_plural = 'User Types'
 
     def __str__(self):
         return self.name
     
-
-#MODEL FOR BUYERS
 class Buyer(models.Model):
-    name = models.CharField(max_length=100)
-    username = models.CharField(max_length=50, unique=True)
-    email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=20)
-    address = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        permissions = [('can_view_buyer', 'Can view buyer'),
+                       ('can_edit_buyer', 'Can edit buyer')]
 
-    def __str__(self):
-        return self.name
+class Seller(models.Model):
+    
+    class Meta:
+        permissions = [('can_view_seller', 'Can view seller'),
+                       ('can_edit_seller', 'Can edit seller')]
+
