7a03e3619c96e81cab88161f06b40732fbf872a5
gagefulwood <gagetfulwood@gmail.com>
2024-03-17 20:32:35 -0500

    Resolved issues with creating shoes

--- shoebae/store/models.py
+++ shoebae/store/models.py
@@ -1,10 +1,58 @@
 from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator, MaxValueValidator, DecimalValidator
+from django.db import models
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 from django.utils import timezone
 from decimal import Decimal 
 
 # Create your models here.
+def increment_by_half_validator(value):
+    if value % 0.5 != 0:
+        raise ValidationError('Value must be incrementable by 0.5.')
+
+class Shoe(models.Model):
+    shoe_name = models.CharField(max_length=255)
+    shoe_image = models.ImageField(upload_to='images/shoes/')
+    shoe_quantity = models.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ]
+    )
+    shoe_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[
+            MinValueValidator(0.01),
+            MaxValueValidator(5000),
+            DecimalValidator(max_digits=10, decimal_places=2)
+        ]
+    )
+    shoe_size = models.DecimalField(
+        max_digits=3,
+        decimal_places=1,
+        validators=[
+            MinValueValidator(1),
+            DecimalValidator(max_digits=3, decimal_places=1)
+        ]
+    )
+    shoe_conditions = models.CharField(
+        max_length=100,
+        default='other'
+    )
+    shoe_brand = models.CharField(
+        max_length=100,
+        default='other'
+    )
+    shoe_category = models.CharField(
+        max_length=255,
+        default='other'
+    )
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.shoe_name
 
 class Condition(models.Model):
     condition_name = models.CharField(max_length=100, unique=True)
@@ -30,59 +78,21 @@ class Category(MPTTModel):
 
     def __str__(self):
         return self.category_name
-
+    
 class Review(models.Model):
+    shoe = models.ForeignKey(Shoe, on_delete=models.CASCADE)
     review_title = models.CharField(max_length=255)
-    review_rating = models.DecimalField(max_digits=2,decimal_places=1)#needs negative prevention and increment of 0.5
+    review_rating = models.DecimalField(
+        max_digits=2,
+        decimal_places=1,
+        validators= [
+            MinValueValidator(1),
+            MaxValueValidator(5),
+            DecimalValidator(max_digits=2,decimal_places=1),
+            increment_by_half_validator
+        ]
+    )
     date_posted = models.DateTimeField(default=timezone.now)
     
     def __str__(self):
         return self.review_title
-
-class Reviews(models.Model):#this container class might be useless
-    review = models.ManyToManyField(Review)
-    #to-do: def __str__(self):
-
-class Shoe(models.Model):
-    #possibly seller
-    shoe_name = models.CharField(max_length=255)
-    shoe_image = models.ImageField(upload_to='images/shoes/')
-    shoe_quantity = models.IntegerField() #needs negative prevention
-    shoe_price = models.DecimalField(max_digits=10,decimal_places=2) #needs negative prevention
-    shoe_size = models.DecimalField(max_digits=3,decimal_places=1)# needs negative prevention and increment of 0.5
-    condition_choices = [(condition.condition_name, str(condition.condition_name)) for condition in Condition.objects.all()]
-    shoe_conditions = models.CharField(#changes to increment drop down thing
-        max_length=100,
-        choices=condition_choices,
-        blank=False, #remove blank and null later
-        null=False
-    )
-    brand_choices = [(brand.brand_name, str(brand.brand_name)) for brand in Brand.objects.all()]
-    shoe_brand = models.CharField(#changes to increment drop down thing
-        max_length=100,
-        choices=brand_choices,
-        blank=False, #remove blank and null later
-        null=False
-    )
-    category_choices = [(category.category_name, (str(category.parent)+" "+str(category.category_name))) for category in Category.objects.all()]
-    shoe_category = models.CharField(#changes to increment drop down thing
-        max_length=255,
-        choices=category_choices,
-        default= "other"
-    )
-    shoe_reviews = models.ManyToManyField(Review, blank=True)
-    
-    date_posted = models.DateTimeField(default=timezone.now)
-
-    def clean(self):
-       def clean(self):
-        if self.shoe_quantity < 0:
-            raise ValidationError("Shoe quantity cannot be negative.")
-        if self.shoe_price < 0:
-            raise ValidationError("Shoe price cannot be negative.")
-        if self.shoe_size is not None:
-            if self.shoe_size < 0 or (self.shoe_size % Decimal('0.5') != 0):
-                raise ValidationError("Shoe size must be a positive multiple of 0.5.")
-    def __str__(self):
-        return self.shoe_name #tweak this to be more specific
-    
