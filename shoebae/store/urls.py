65ca65f1a200ec07f6ecd1640423e99185903218
citlalih1421 <citlali.h1421@gmail.com>
2024-03-20 18:30:15 -0500

    Added images and html files

--- shoebae/store/urls.py
+++ shoebae/store/urls.py
@@ -4,5 +4,9 @@ urlpatterns = [
     #empty for base url
     path('', views.store, name="store"),
     path('cart/', views.cart, name="cart"),
-    path('checkout/', views.checkout, name="checkout")
+    path('checkout/', views.checkout, name="checkout"),
+    path('home/', views.home, name="home"),
+    path('addisting/', views.addLisiting, name="addlisting"),
+    path('productpage/', views.productPage, name="productpage"),
+    path('seller/', views.seller, name="seller"),
 ]
